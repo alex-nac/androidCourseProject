02ec3aa0757f95691ff17fa194721a7178a28204
Alex <alex.nac@yandex.ru>
2015-11-07 21:55:22 +0300

    sample

---
+++ android/src/com/bubblezombie/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bubblezombie.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bubblezombie.game.androidCourseProject;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new androidCourseProject(), config);
+	}
+}
