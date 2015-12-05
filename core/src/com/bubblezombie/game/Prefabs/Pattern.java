a39bd3791aefcb808f5f241c7eb0eb1984102e98
artemZholus <zholus@rain.ifmo.ru>
2015-12-06 01:30:03 +0300

    prefab package seems to be done

--- core/src/com/bubblezombie/game/Prefabs/Pattern.java
+++ core/src/com/bubblezombie/game/Prefabs/Pattern.java
@@ -5,27 +5,29 @@ import com.bubblezombie.game.Util.Pair;
 import java.util.ArrayList;
 
 public class Pattern {
-    ArrayList<Integer> prefabTypes = new ArrayList<Integer>();
-    ArrayList<Integer> prefabProbability = new ArrayList<Integer>();
+
+    // prefab types and prefab probability should be packege-protected because they can be assigned in other classes in package
+    private ArrayList<Integer> _prefabTypes = new ArrayList<Integer>();
+    private ArrayList<Integer> _prefabProbability = new ArrayList<Integer>();
 
     public int count;
     public int minDistance;
     public boolean canOverlay;
     public int firstMaxIndex;
 
-    private int lastPrefabUsed = -1;
+    private int _lastPrefabUsed = -1;
 
     public void setPrefabTypes(ArrayList<Integer> value) {
-        this.prefabTypes = value;
+        this._prefabTypes = value;
     }
     public void setPrefabProbability(ArrayList<Integer> value) {
-        this.prefabProbability = value;
+        this._prefabProbability = value;
     }
 
     public Pair<Integer, Integer> GetNextPrefabTypeAndProb() {
-        lastPrefabUsed++;
-        if (lastPrefabUsed == prefabTypes.size())
-            lastPrefabUsed = 0;
-        return new Pair<Integer, Integer>(prefabTypes.get(lastPrefabUsed), prefabProbability.get(lastPrefabUsed));
+        _lastPrefabUsed++;
+        if (_lastPrefabUsed == _prefabTypes.size())
+            _lastPrefabUsed = 0;
+        return new Pair<Integer, Integer>(_prefabTypes.get(_lastPrefabUsed), _prefabProbability.get(_lastPrefabUsed));
     }
 }
--- core/src/com/bubblezombie/game/Prefabs/PatternData.java
+++ core/src/com/bubblezombie/game/Prefabs/PatternData.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PatternData {
-    private ArrayList<Pattern> patterns = new ArrayList<Pattern>();
+    private ArrayList<Pattern> _patterns = new ArrayList<Pattern>();
 
     public PatternData() {}
 
-    private List<Integer> GetArrayFromString(String str) {
+    private List<Integer> getArrayFromString(String str) {
         ArrayList<Integer> res = new ArrayList<Integer>();
         String[] splited = str.split("_+");
         for (String aSplited : splited) {
@@ -19,25 +19,25 @@ public class PatternData {
         return res;
     }
 
-    public void AddPattern(int firstMaxIndex, String prefabTypes, String prefabProbability, int count, int minDistance, boolean canOverlay) {
+    public void addPattern(int firstMaxIndex, String prefabTypes, String prefabProbability, int count, int minDistance, boolean canOverlay) {
         Pattern pattern = new Pattern();
         pattern.firstMaxIndex = firstMaxIndex;
-        pattern.prefabTypes = new ArrayList<Integer>(GetArrayFromString(prefabTypes));
+        pattern.setPrefabTypes(new ArrayList<Integer>(getArrayFromString(prefabTypes)));
         // TODO: probability is real number
-        pattern.prefabProbability = new ArrayList<Integer>(GetArrayFromString(prefabProbability));
+        pattern.setPrefabProbability(new ArrayList<Integer>(getArrayFromString(prefabProbability)));
         pattern.count = count;
         pattern.minDistance = minDistance;
         pattern.canOverlay = canOverlay;
 
-        patterns.add(pattern);
+        _patterns.add(pattern);
     }
 
-    public Pattern GetRandomPattern() {
-        if (patterns.size() == 0) {
+    public Pattern getRandomPattern() {
+        if (_patterns.size() == 0) {
             return null;
         }
-        int ind = Generator.rand(patterns.size());
-        return patterns.get(ind);
+        int ind = Generator.rand(_patterns.size());
+        return _patterns.get(ind);
     }
 
 }
