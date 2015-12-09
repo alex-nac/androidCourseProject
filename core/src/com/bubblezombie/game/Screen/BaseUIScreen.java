cf223e158ab5351aa9eb3f9e875e8a24c4a53474
Alex <alex.nac@yandex.ru>
2015-12-10 00:06:28 +0300

    Base UI classes and some UI assets

---
+++ core/src/com/bubblezombie/game/Screen/BaseUIScreen.java
@@ -0,0 +1,94 @@
+package com.bubblezombie.game.Screen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+/**
+ * Created by Alex on 7/12/15.
+ * Handy class for creating basement of all menu screens:
+ * ctor take booleans as a parameters in order to determine
+ * which buttons we should display
+ */
+public class BaseUIScreen extends BaseScreen {
+    private boolean _isMoreGamesBtn, _isBackBtn, _isAchievmentsBtn, _isRestartBtn,
+            _isResetBtn, _isSkipLevelBtn, _isNextLvlBtn, _isLvlMapBtn;
+
+    // TODO: may be protected interface?? ex: protected GetBackBtn() { if (!_isBackBtn) throw Exception("No such btn") }
+    protected Button moreGamesBtn, backBtn, achievmentsBtn, restartBtn,
+            resetBtn, skipLevelBtn, nextLvlBtn, lvlMapBtn;
+    protected
+
+    BaseUIScreen(Game game, boolean isMoreGamesBtn, boolean isBackBtn, boolean isAchievmentsBtn, boolean isRestartBtn,
+                 boolean isResetBtn, boolean isSkipLevelBtn, boolean isNextLvlBtn, boolean isLvlMapBtn) {
+        super(game);
+        _isMoreGamesBtn = isMoreGamesBtn;
+        _isBackBtn = isBackBtn;
+        _isAchievmentsBtn = isAchievmentsBtn;
+        _isRestartBtn = isRestartBtn;
+        _isResetBtn = isResetBtn;
+        _isSkipLevelBtn = isSkipLevelBtn;
+        _isNextLvlBtn = isNextLvlBtn;
+        _isLvlMapBtn = isLvlMapBtn;
+    }
+
+    @Override
+    public void show() {
+        Image BGsprite = new Image(new Texture(Gdx.files.internal("background/menu.png")));
+        stage.addActor(BGsprite);
+
+        // TODO: remember - we should have only one background for button
+
+        // buttons:
+
+        TextButton.TextButtonStyle tbs;
+
+        // more games button
+        if (_isMoreGamesBtn) {
+            tbs = new TextButton.TextButtonStyle();
+            tbs.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("background/UI_buttons/but_mg_level_map.png"))));
+            Button moreGamesBtn = new Button(tbs);
+            moreGamesBtn.setPosition(17.0f, 415.0f);
+            moreGamesBtn.setHeight(45.0f);
+            moreGamesBtn.setWidth(45.0f);
+            stage.addActor(moreGamesBtn);
+        }
+
+        // level map button
+        if (_isLvlMapBtn) {
+            tbs = new TextButton.TextButtonStyle();
+            tbs.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("background/UI_buttons/but_menu.png"))));
+            Button lvlMapBtn = new Button(tbs);
+            lvlMapBtn.setPosition(103.0f, 7.0f);
+            lvlMapBtn.setHeight(63.0f);
+            lvlMapBtn.setWidth(63.0f);
+            stage.addActor(lvlMapBtn);
+        }
+
+        // next level button
+        if (_isNextLvlBtn) {
+            tbs = new TextButton.TextButtonStyle();
+            tbs.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("background/UI_buttons/but_menu.png"))));
+            Button nextScreenBtn = new Button(tbs);
+            nextScreenBtn.setPosition(103.0f, 7.0f);
+            nextScreenBtn.setHeight(63.0f);
+            nextScreenBtn.setWidth(63.0f);
+            stage.addActor(nextScreenBtn);
+        }
+
+        // sound ON button
+        tbs = new TextButton.TextButtonStyle();
+        tbs.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("background/UI_buttons/but_menu.png"))));
+        Button lvlMapBtn = new Button(tbs);
+        lvlMapBtn.setPosition(103.0f, 7.0f);
+        lvlMapBtn.setHeight(63.0f);
+        lvlMapBtn.setWidth(63.0f);
+        stage.addActor(lvlMapBtn);
+    }
+
+}
---
+++ core/src/com/bubblezombie/game/Screen/IntroScreen.java
@@ -0,0 +1,41 @@
+package com.bubblezombie.game.Screen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.bubblezombie.game.BubbleZombieGame;
+
+/**
+ *  Created by Alex on 05/12/15.
+ *  Intro state class, the game intro is shown after the PLAY button is pressed
+ */
+
+public class IntroScreen extends BaseScreen {
+
+    IntroScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+
+        // TODO: real font, not just image
+        Image introContent = new Image(new Texture(Gdx.files.internal("background/intro_content.png")));
+        introContent.setPosition((BubbleZombieGame.width - introContent.getWidth()) / 2,
+                (BubbleZombieGame.height - introContent.getHeight()) / 2);
+        stage.addActor(introContent);
+
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
