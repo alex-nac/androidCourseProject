bafca874d9e16c2fc6e85ed505662fe247bea76d
artemZholus <zholus@rain.ifmo.ru>
2016-01-14 15:37:04 +0300

    cbtype

---
+++ core/src/com/bubblezombie/game/CBType.java
@@ -0,0 +1,10 @@
+package com.bubblezombie.game;
+
+import java.io.Serializable;
+
+public class CBType {
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ core/src/com/bubblezombie/game/CBTypebody.java
@@ -0,0 +1,36 @@
+package com.bubblezombie.game;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.util.HashSet;
+
+public class CBTypebody extends Body {
+
+    private static HashSet<CBType> set = new HashSet<CBType>();
+
+    /**
+     * Constructs a new body with the given address
+     *
+     * @param world the world
+     * @param addr  the address
+     */
+    protected CBTypebody(World world, long addr) {
+        super(world, addr);
+    }
+
+    public void addCbtype(CBType element) {
+        set.add(element);
+    }
+
+    public void removeCbtype(CBType element) {
+        if (set.contains(element)) {
+            set.remove(element);
+        }
+    }
+
+    public boolean hasCbtype(CBType element) {
+        return set.contains(element);
+    }
+
+}
