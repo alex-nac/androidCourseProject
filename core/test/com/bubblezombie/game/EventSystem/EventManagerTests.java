73bd999556c1877f67be272f56569beb5d4df2ee
Alexandr Naconechny <alex.nac@yandex.ru>
2017-01-05 21:15:35 +0300

    tests

---
+++ core/test/com/bubblezombie/game/EventSystem/EventManagerTests.java
@@ -0,0 +1,18 @@
+package com.bubblezombie.game.EventSystem;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class EventManagerTests {
+    @Test
+    public void thisAlwaysPasses() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
